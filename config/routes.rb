f7bbcbd00be618fbfd7dd59868adde39cc9bf32e
nshpam <pamza2545@gmail.com>
2023-09-25 10:25:52 +0700

    omni oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rottenpotatoes::Application.routes.draw do
+  devise_for :moviegoers, controllers: {
+    omniauth_callbacks: 'moviegoers/omniauth_callbacks',
+    sessions: 'moviegoers/sessions',
+    registrations: 'moviegoers/registrations'
+    }
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+
+  
+
 end
